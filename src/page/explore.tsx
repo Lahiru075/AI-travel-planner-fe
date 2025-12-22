8bf58a1c68a3df7120bdadb9d27b6b16cd6ce82c
Lahiru075 <sanjeewalahiru057@gmail.com>
2025-12-22 18:04:35 +0530

    complete all core functionalities

--- src/page/explore.tsx
+++ src/page/explore.tsx
@@ -90,7 +90,7 @@ const Explore = () => {
                     </div>
                     <input
                         type="text"
-                        placeholder="Search destination (e.g. Japan, Paris)..."
+                        placeholder="Search destination (e.g. Nuwara Eliya, Haputale)..."
                         className="w-full pl-10 pr-4 py-3 bg-slate-900/50 border border-slate-700 rounded-xl text-white placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all shadow-lg backdrop-blur-sm"
                         value={searchQuery}
                         onChange={handleSearchChange} // 👇 අලුත් Handler එක
